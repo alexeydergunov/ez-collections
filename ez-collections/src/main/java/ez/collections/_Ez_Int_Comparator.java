cd39f1ca34c8e3ff6a5aed2edced42067674a7bd
Alexey Dergunov <alexey.dergunov@gmail.com>
2014-08-31 11:52:58 +0400

    Javadocs for Comparator

--- ez-collections/src/main/java/ez/collections/_Ez_Int_Comparator.java
+++ ez-collections/src/main/java/ez/collections/_Ez_Int_Comparator.java
@@ -1,6 +1,20 @@
 package ez.collections;
 
-// TODO javadocs
+/**
+ * An analogue of {@link java.util.Comparator}, an interface which is used to define a custom ordering for the elements.
+ * @author Alexey Dergunov
+ * @since 0.0.1
+ * @see ez.collections.sort._Ez_Int_Sort
+ * @see ez.collections.heap._Ez_Int_CustomHeap
+ */
 public interface _Ez_Int_Comparator {
+    /**
+     * A comparison function. It accepts two parameters {@code a} and {@code b} and must return a negative integer if
+     * {@code a < b} (in terms of your custom ordering), a positive integer if {@code a > b}, and a zero if they are
+     * equal.
+     * @param a the first argument
+     * @param b the second argument
+     * @return a negative integer if {@code a < b}, a positive integer if {@code a > b}, and a zero if they are equal
+     */
     int compare(/*C*/int/*C*/ a, /*C*/int/*C*/ b);
 }
