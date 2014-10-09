92ebe398f6e01e60a6f052b7eab506c0718bc874
Alexey Dergunov <alexey.dergunov@gmail.com>
2014-10-09 22:45:32 +0400

    Added HashMap

---
+++ ez-collections/src/main/java/ez/collections/hashmap/_Ez_Int__Int_HashMap.java
@@ -0,0 +1,349 @@
+package ez.collections.hashmap;
+
+import ez.collections._Ez_Int__Int_Map;
+import ez.collections._Ez_Int__Int_MapIterator;
+import ez.collections.misc.PrimitiveHashCalculator;
+
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+import java.util.Random;
+
+public class _Ez_Int__Int_HashMap implements _Ez_Int__Int_Map {
+    private static final int DEFAULT_CAPACITY = 8;
+    private static final double ENLARGE_NON_FREE_CELLS_RATIO = 0.5;
+    private static final double REBUILD_REMOVED_CELLS_RATIO = 0.25;
+    private static final int HASHCODE_INITIAL_VALUE = 0x811c9dc5;
+    private static final int HASHCODE_MULTIPLIER = 0x01000193;
+
+    private static final byte FREE = 0;
+    private static final byte REMOVED = 1;
+    private static final byte FILLED = 2;
+
+    private static final /*V*/int/*V*/ DEFAULT_NULL_VALUE = (new /*V*/int/*V*/[1])[0];
+
+    private static final Random rnd = new Random();
+    private static final int POS_RANDOM_SHIFT_1;
+    private static final int POS_RANDOM_SHIFT_2;
+    private static final int STEP_RANDOM_SHIFT_1;
+    private static final int STEP_RANDOM_SHIFT_2;
+
+    static {
+        POS_RANDOM_SHIFT_1 = rnd.nextInt(10) + 11;
+        POS_RANDOM_SHIFT_2 = rnd.nextInt(10) + 21;
+        STEP_RANDOM_SHIFT_1 = rnd.nextInt(10) + 11;
+        STEP_RANDOM_SHIFT_2 = rnd.nextInt(10) + 21;
+    }
+
+    private /*K*/int/*K*/[] keys;
+    private /*V*/int/*V*/[] values;
+    private byte[] status;
+    private int size;
+    private int removedCount;
+    private int mask;
+    private boolean returnedNull;
+    private final int hashSeed;
+
+    public _Ez_Int__Int_HashMap() {
+        this(DEFAULT_CAPACITY);
+    }
+
+    public _Ez_Int__Int_HashMap(int capacity) {
+        if (capacity < 0) {
+            throw new IllegalArgumentException("Capacity must be non-negative");
+        }
+        capacity = Math.max(4, (int) (capacity / ENLARGE_NON_FREE_CELLS_RATIO));
+        if ((capacity & (capacity - 1)) != 0) {
+            capacity = Integer.highestOneBit(capacity) << 1;
+        }
+        // Capacity is a power of 2 now
+        keys = new /*K*/int/*K*/[capacity];
+        values = new /*V*/int/*V*/[capacity];
+        status = new byte[capacity];
+        size = 0;
+        removedCount = 0;
+        mask = capacity - 1;
+        returnedNull = false;
+        hashSeed = rnd.nextInt();
+    }
+
+    private int getStartPos(int h) {
+        h ^= hashSeed;
+        h ^= (h >>> POS_RANDOM_SHIFT_1) ^ (h >>> POS_RANDOM_SHIFT_2);
+        return h & mask;
+    }
+
+    private int getStep(int h) {
+        h ^= hashSeed;
+        h ^= (h >>> STEP_RANDOM_SHIFT_1) ^ (h >>> STEP_RANDOM_SHIFT_2);
+        return ((h << 1) | 1) & mask;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public boolean containsKey(/*K*/int/*K*/ key) {
+        final int keyHash = PrimitiveHashCalculator.getHash(key);
+        int pos = getStartPos(keyHash);
+        final int step = getStep(keyHash);
+        for (; status[pos] != FREE; pos = (pos + step) & mask) {
+            if (status[pos] == FILLED && keys[pos] == key) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public /*V*/int/*V*/ get(/*K*/int/*K*/ key) {
+        final int keyHash = PrimitiveHashCalculator.getHash(key);
+        int pos = getStartPos(keyHash);
+        final int step = getStep(keyHash);
+        for (; status[pos] != FREE; pos = (pos + step) & mask) {
+            if (status[pos] == FILLED && keys[pos] == key) {
+                returnedNull = false;
+                return values[pos];
+            }
+        }
+        returnedNull = true;
+        return DEFAULT_NULL_VALUE;
+    }
+
+    @Override
+    public /*V*/int/*V*/ put(/*K*/int/*K*/ key, /*V*/int/*V*/ value) {
+        final int keyHash = PrimitiveHashCalculator.getHash(key);
+        int pos = getStartPos(keyHash);
+        final int step = getStep(keyHash);
+        for (; status[pos] == FILLED; pos = (pos + step) & mask) {
+            if (keys[pos] == key) {
+                final /*V*/int/*V*/ oldValue = values[pos];
+                values[pos] = value;
+                returnedNull = false;
+                return oldValue;
+            }
+        }
+        if (status[pos] == FREE) {
+            status[pos] = FILLED;
+            keys[pos] = key;
+            values[pos] = value;
+            size++;
+            if (size + removedCount > keys.length * ENLARGE_NON_FREE_CELLS_RATIO) {
+                rebuild(keys.length * 2);
+            }
+            returnedNull = true;
+            return DEFAULT_NULL_VALUE;
+        }
+        final int removedPos = pos;
+        for (pos = (pos + step) & mask; status[pos] != FREE; pos = (pos + step) & mask) {
+            if (status[pos] == FILLED && keys[pos] == key) {
+                final /*V*/int/*V*/ oldValue = values[pos];
+                values[pos] = value;
+                returnedNull = false;
+                return oldValue;
+            }
+        }
+        status[removedPos] = FILLED;
+        keys[removedPos] = key;
+        values[removedPos] = value;
+        size++;
+        removedCount--;
+        returnedNull = true;
+        return DEFAULT_NULL_VALUE;
+    }
+
+    @Override
+    public /*V*/int/*V*/ remove(/*K*/int/*K*/ key) {
+        final int keyHash = PrimitiveHashCalculator.getHash(key);
+        int pos = getStartPos(keyHash);
+        final int step = getStep(keyHash);
+        for (; status[pos] != FREE; pos = (pos + step) & mask) {
+            if (status[pos] == FILLED && keys[pos] == key) {
+                final /*V*/int/*V*/ removedValue = values[pos];
+                status[pos] = REMOVED;
+                size--;
+                removedCount++;
+                if (removedCount > keys.length * REBUILD_REMOVED_CELLS_RATIO) {
+                    rebuild(keys.length);
+                }
+                returnedNull = false;
+                return removedValue;
+            }
+        }
+        returnedNull = true;
+        return DEFAULT_NULL_VALUE;
+    }
+
+    @Override
+    public boolean returnedNull() {
+        return returnedNull;
+    }
+
+    @Override
+    public void clear() {
+        Arrays.fill(status, FREE);
+        size = 0;
+        removedCount = 0;
+    }
+
+    @Override
+    public /*K*/int/*K*/[] keys() {
+        /*K*/int/*K*/[] result = new /*K*/int/*K*/[size];
+        for (int i = 0, j = 0; i < keys.length; i++) {
+            if (status[i] == FILLED) {
+                result[j++] = keys[i];
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public /*V*/int/*V*/[] values() {
+        /*V*/int/*V*/[] result = new /*V*/int/*V*/[size];
+        for (int i = 0, j = 0; i < values.length; i++) {
+            if (status[i] == FILLED) {
+                result[j++] = values[i];
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public _Ez_Int__Int_MapIterator iterator() {
+        return new _Ez_Int__Int_HashMapIterator();
+    }
+
+    private void rebuild(int newCapacity) {
+        /*K*/int/*K*/[] oldKeys = keys;
+        /*V*/int/*V*/[] oldValues = values;
+        byte[] oldStatus = status;
+        keys = new /*K*/int/*K*/[newCapacity];
+        values = new /*V*/int/*V*/[newCapacity];
+        status = new byte[newCapacity];
+        size = 0;
+        removedCount = 0;
+        mask = newCapacity - 1;
+        for (int i = 0; i < oldKeys.length; i++) {
+            if (oldStatus[i] == FILLED) {
+                put(oldKeys[i], oldValues[i]);
+            }
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        _Ez_Int__Int_HashMap that = (_Ez_Int__Int_HashMap) o;
+
+        if (size != that.size) {
+            return false;
+        }
+        for (int i = 0; i < keys.length; i++) {
+            if (status[i] == FILLED) {
+                /*V*/int/*V*/ thatValue = that.get(keys[i]);
+                if (that.returnedNull || thatValue != values[i]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private void randomShuffle(int[] array) {
+        int n = array.length;
+        for (int i = 0; i < n; i++) {
+            int j = i + rnd.nextInt(n - i);
+            int tmp = array[i];
+            array[i] = array[j];
+            array[j] = tmp;
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        int[] entryHashes = new int[size];
+        for (int i = 0, j = 0; i < status.length; i++) {
+            if (status[i] == FILLED) {
+                int hash = HASHCODE_INITIAL_VALUE;
+                hash = (hash ^ PrimitiveHashCalculator.getHash(keys[i])) * HASHCODE_MULTIPLIER;
+                hash = (hash ^ PrimitiveHashCalculator.getHash(values[i])) * HASHCODE_MULTIPLIER;
+                entryHashes[j++] = hash;
+            }
+        }
+        randomShuffle(entryHashes);
+        Arrays.sort(entryHashes);
+        int hash = HASHCODE_INITIAL_VALUE;
+        for (int i = 0; i < size; i++) {
+            hash = (hash ^ entryHashes[i]) * HASHCODE_MULTIPLIER;
+        }
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append('{');
+        for (int i = 0; i < keys.length; i++) {
+            if (status[i] == FILLED) {
+                if (sb.length() > 1) {
+                    sb.append(", ");
+                }
+                sb.append(keys[i]);
+                sb.append('=');
+                sb.append(values[i]);
+            }
+        }
+        sb.append('}');
+        return sb.toString();
+    }
+
+    private class _Ez_Int__Int_HashMapIterator implements _Ez_Int__Int_MapIterator {
+        private int curIndex;
+
+        private _Ez_Int__Int_HashMapIterator() {
+            curIndex = 0;
+            while (curIndex < status.length && status[curIndex] != FILLED) {
+                curIndex++;
+            }
+        }
+
+        @Override
+        public boolean hasNext() {
+            return curIndex < status.length;
+        }
+
+        @Override
+        public /*K*/int/*K*/ getKey() {
+            if (curIndex == keys.length) {
+                throw new NoSuchElementException("Iterator doesn't have more elements");
+            }
+            return keys[curIndex];
+        }
+
+        @Override
+        public /*V*/int/*V*/ getValue() {
+            if (curIndex == values.length) {
+                throw new NoSuchElementException("Iterator doesn't have more elements");
+            }
+            return values[curIndex];
+        }
+
+        @Override
+        public void next() {
+            if (curIndex == status.length) {
+                throw new NoSuchElementException("Iterator doesn't have more elements");
+            }
+            curIndex++;
+            while (curIndex < status.length && status[curIndex] != FILLED) {
+                curIndex++;
+            }
+        }
+    }
+}
---
+++ ez-collections/src/test/java/ez/collections/hashmap/EzHashMapTest.java
@@ -0,0 +1,311 @@
+package ez.collections.hashmap;
+
+import ez.collections._Ez_Int__Int_MapIterator;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.NoSuchElementException;
+import java.util.Random;
+
+public class EzHashMapTest {
+    private void assertKeyData(_Ez_Int__Int_HashMap ezMap, HashMap<Integer, Integer> javaMap, int key) {
+        Integer javaVal = javaMap.get(key);
+        int ezVal = ezMap.get(key);
+        if (javaVal == null) {
+            Assert.assertTrue(ezMap.returnedNull());
+        } else {
+            Assert.assertFalse(ezMap.returnedNull());
+            Assert.assertEquals(ezVal, (int) javaVal);
+        }
+        Assert.assertEquals(ezMap.containsKey(key), javaMap.containsKey(key));
+    }
+
+    @Test
+    public void testPutGetRemoveContainsSmallMaps() {
+        Random rnd = new Random(322);
+        for (int it = 0; it < 10000; it++) {
+            _Ez_Int__Int_HashMap ezMap = new _Ez_Int__Int_HashMap(0);
+            HashMap<Integer, Integer> javaMap = new HashMap<Integer, Integer>();
+            Assert.assertTrue(ezMap.isEmpty());
+            for (int i = 0; i < 100; i++) {
+                int key = rnd.nextInt(50);
+                int value = rnd.nextInt(50);
+                assertKeyData(ezMap, javaMap, key);
+                Assert.assertEquals(ezMap.size(), javaMap.size());
+                if (rnd.nextBoolean()) {
+                    Integer javaVal = javaMap.put(key, value);
+                    int ezVal = ezMap.put(key, value);
+                    if (javaVal == null) {
+                        Assert.assertTrue(ezMap.returnedNull());
+                    } else {
+                        Assert.assertFalse(ezMap.returnedNull());
+                        Assert.assertEquals(ezVal, (int) javaVal);
+                    }
+                } else {
+                    Integer javaVal = javaMap.remove(key);
+                    int ezVal = ezMap.remove(key);
+                    if (javaVal == null) {
+                        Assert.assertTrue(ezMap.returnedNull());
+                    } else {
+                        Assert.assertFalse(ezMap.returnedNull());
+                        Assert.assertEquals(ezVal, (int) javaVal);
+                    }
+                }
+                assertKeyData(ezMap, javaMap, key);
+                Assert.assertEquals(ezMap.size(), javaMap.size());
+            }
+        }
+    }
+
+    @Test
+    public void testPutGetRemoveContainsBigMaps() {
+        Random rnd = new Random(3223);
+        for (int it = 0; it < 100; it++) {
+            _Ez_Int__Int_HashMap ezMap = new _Ez_Int__Int_HashMap(0);
+            HashMap<Integer, Integer> javaMap = new HashMap<Integer, Integer>();
+            Assert.assertTrue(ezMap.isEmpty());
+            for (int i = 0; i < 10000; i++) {
+                int key = rnd.nextInt(5000);
+                int value = rnd.nextInt(5000);
+                assertKeyData(ezMap, javaMap, key);
+                Assert.assertEquals(ezMap.size(), javaMap.size());
+                if (rnd.nextBoolean()) {
+                    Integer javaVal = javaMap.put(key, value);
+                    int ezVal = ezMap.put(key, value);
+                    if (javaVal == null) {
+                        Assert.assertTrue(ezMap.returnedNull());
+                    } else {
+                        Assert.assertFalse(ezMap.returnedNull());
+                        Assert.assertEquals(ezVal, (int) javaVal);
+                    }
+                } else {
+                    Integer javaVal = javaMap.remove(key);
+                    int ezVal = ezMap.remove(key);
+                    if (javaVal == null) {
+                        Assert.assertTrue(ezMap.returnedNull());
+                    } else {
+                        Assert.assertFalse(ezMap.returnedNull());
+                        Assert.assertEquals(ezVal, (int) javaVal);
+                    }
+                }
+                assertKeyData(ezMap, javaMap, key);
+                Assert.assertEquals(ezMap.size(), javaMap.size());
+            }
+        }
+    }
+
+    @Test
+    public void testPut() {
+        _Ez_Int__Int_HashMap map = new _Ez_Int__Int_HashMap();
+        int[] array = new int[10000];
+        Arrays.fill(array, -1);
+        Random rnd = new Random(32232);
+        int size = 0;
+        for (int i = 0; i < 1000000; i++) {
+            Assert.assertEquals(map.size(), size);
+            int key = rnd.nextInt(10000);
+            int oldValue = map.put(key, i);
+            if (array[key] == -1) {
+                size++;
+                Assert.assertTrue(map.returnedNull());
+            } else {
+                Assert.assertFalse(map.returnedNull());
+                Assert.assertEquals(oldValue, array[key]);
+            }
+            array[key] = i;
+            Assert.assertEquals(map.size(), size);
+        }
+    }
+
+    @Test
+    public void testGetContainsKey() {
+        _Ez_Int__Int_HashMap map = new _Ez_Int__Int_HashMap();
+        Random rnd = new Random(322322);
+        int size = 100000;
+        int[] array = new int[size];
+        for (int i = 0; i < size; i++) {
+            array[i] = rnd.nextInt();
+            map.put(i, array[i]);
+            Assert.assertTrue(map.returnedNull());
+        }
+        Assert.assertEquals(map.size(), size);
+        for (int i = 0; i < size; i++) {
+            Assert.assertEquals(map.get(i), array[i]);
+            Assert.assertFalse(map.returnedNull());
+            Assert.assertTrue(map.containsKey(i));
+            map.get(~i);
+            Assert.assertTrue(map.returnedNull());
+            Assert.assertFalse(map.containsKey(~i));
+        }
+    }
+
+    @Test
+    public void testRemove() {
+        _Ez_Int__Int_HashMap map = new _Ez_Int__Int_HashMap();
+        Random rnd = new Random(3223223);
+        int size = 100000;
+        int[] array = new int[size];
+        for (int i = 0; i < size; i++) {
+            array[i] = rnd.nextInt();
+            map.put(i, array[i]);
+            Assert.assertTrue(map.returnedNull());
+        }
+        Assert.assertEquals(map.size(), size);
+        for (int i = 0; i < size; i++) {
+            Assert.assertTrue(map.containsKey(i));
+            Assert.assertEquals(map.remove(i), array[i]);
+            Assert.assertFalse(map.returnedNull());
+            Assert.assertFalse(map.containsKey(i));
+            map.remove(i);
+            Assert.assertTrue(map.returnedNull());
+            Assert.assertFalse(map.containsKey(i));
+        }
+    }
+
+    @Test
+    public void testClear() {
+        _Ez_Int__Int_HashMap map = new _Ez_Int__Int_HashMap();
+        Assert.assertTrue(map.isEmpty());
+        Assert.assertEquals(map.size(), 0);
+        int size = 0;
+        for (int i = 1; i <= 1000000; i++) {
+            map.put(i, -i);
+            size++;
+            Assert.assertFalse(map.isEmpty());
+            Assert.assertEquals(map.size(), size);
+            if ((i & (i - 1)) == 0) {
+                map.clear();
+                Assert.assertTrue(map.isEmpty());
+                Assert.assertEquals(map.size(), 0);
+                size = 0;
+            }
+        }
+    }
+
+    @Test
+    public void testKeysValuesArrays() {
+        Random rnd = new Random(32232232);
+        int n = 100000;
+        int[] keys = new int[n];
+        int[] values = new int[n];
+        for (int i = 0; i < n; i++) {
+            keys[i] = (1 + i) * 10000 + rnd.nextInt(9000);
+            values[i] = rnd.nextInt();
+        }
+        _Ez_Int__Int_HashMap map = new _Ez_Int__Int_HashMap();
+        for (int i = 0; i < n; i++) {
+            map.put(keys[i], values[i]);
+            Assert.assertTrue(map.returnedNull());
+        }
+        Assert.assertEquals(map.size(), n);
+        int[] mapKeys = map.keys();
+        int[] mapValues = map.values();
+        Arrays.sort(keys);
+        Arrays.sort(mapKeys);
+        Arrays.sort(values);
+        Arrays.sort(mapValues);
+        Assert.assertEquals(mapKeys, keys);
+        Assert.assertEquals(mapValues, values);
+    }
+
+    @Test
+    public void testIterator() {
+        Random rnd = new Random(322322322);
+        int n = 1000;
+        int[] array = new int[n];
+        for (int i = 0; i < n; i++) {
+            array[i] = rnd.nextInt();
+        }
+        _Ez_Int__Int_HashMap map = new _Ez_Int__Int_HashMap();
+        for (int i = 0; i < n; i++) {
+            map.put(i, array[i]);
+            Assert.assertTrue(map.returnedNull());
+        }
+        boolean[] visited = new boolean[n];
+        _Ez_Int__Int_MapIterator it = map.iterator();
+        for (int i = 0; i < n; i++) {
+            Assert.assertTrue(it.hasNext());
+            int key = it.getKey();
+            int value = it.getValue();
+            Assert.assertEquals(value, array[key]);
+            Assert.assertFalse(visited[key]);
+            visited[key] = true;
+            for (int retries = 0; retries < 2; retries++) {
+                Assert.assertTrue(it.hasNext());
+                Assert.assertEquals(it.getKey(), key);
+                Assert.assertEquals(it.getValue(), value);
+            }
+            it.next();
+        }
+        for (int retries = 0; retries < 3; retries++) {
+            Assert.assertFalse(it.hasNext());
+            try {
+                it.getKey();
+            } catch (NoSuchElementException e) {
+                // as expected
+            }
+            try {
+                it.getValue();
+            } catch (NoSuchElementException e) {
+                // as expected
+            }
+            try {
+                it.next();
+            } catch (NoSuchElementException e) {
+                // as expected
+            }
+        }
+    }
+
+    @Test
+    public void testEqualsHashcode() {
+        for (int mask1 = 0; mask1 < 32; mask1++) {
+            for (int mask2 = 0; mask2 < 32; mask2++) {
+                _Ez_Int__Int_HashMap map1 = new _Ez_Int__Int_HashMap(1);
+                _Ez_Int__Int_HashMap map2 = new _Ez_Int__Int_HashMap(1);
+                for (int i = 0; i < 5; i++) {
+                    if ((mask1 & (1 << i)) != 0) {
+                        map1.put(i, 1000 * i);
+                    }
+                    if ((mask2 & (1 << i)) != 0) {
+                        map2.put(i, 1000 * i);
+                    }
+                }
+                int hashCode1 = map1.hashCode();
+                int hashCode2 = map2.hashCode();
+                if (mask1 == mask2) {
+                    Assert.assertTrue(map1.equals(map2));
+                    Assert.assertTrue(map2.equals(map1));
+                    Assert.assertTrue(hashCode1 == hashCode2);
+                } else {
+                    Assert.assertFalse(map1.equals(map2));
+                    Assert.assertFalse(map2.equals(map1));
+                }
+            }
+        }
+    }
+
+    @Test
+    public void testToString() {
+        _Ez_Int__Int_HashMap map = new _Ez_Int__Int_HashMap();
+        assertIsOneOf(map.toString(), "{}");
+        map.put(123, 456);
+        assertIsOneOf(map.toString(), "{123=456}");
+        map.put(-987, -654);
+        assertIsOneOf(map.toString(), "{123=456, -987=-654}", "{-987=-654, 123=456}");
+    }
+
+    private void assertIsOneOf(Object actual, Object... expected) {
+        for (Object e : expected) {
+            if (e == null && actual == null) {
+                return;
+            }
+            if (e != null && e.equals(actual)) {
+                return;
+            }
+        }
+        Assert.fail();
+    }
+}
