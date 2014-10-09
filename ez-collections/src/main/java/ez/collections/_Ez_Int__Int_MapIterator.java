b77378af1d714c3804fb2407fa14deb3d07652b3
Alexey Dergunov <alexey.dergunov@gmail.com>
2014-10-09 22:35:06 +0400

    Reworked methods in MapIterator: advance() changed to next()

--- ez-collections/src/main/java/ez/collections/_Ez_Int__Int_MapIterator.java
+++ ez-collections/src/main/java/ez/collections/_Ez_Int__Int_MapIterator.java
@@ -3,9 +3,9 @@ package ez.collections;
 public interface _Ez_Int__Int_MapIterator {
     boolean hasNext();
 
-    void advance();
-
     /*K*/int/*K*/ getKey();
 
     /*V*/int/*V*/ getValue();
+
+    void next();
 }
